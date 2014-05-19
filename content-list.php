68395e175ed78d32fdc195048ee4371fa104929e
David Goss <david@davidgoss.co.uk>
2014-05-20 00:22:52 +0100

    Add Microdata to markup for post lists

--- content-list.php
+++ content-list.php
@@ -6,17 +6,17 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>">
+<article id="post-<?php the_ID(); ?>" itemscope itemtype="http://schema.org/BlogPosting">
 	<header>
-		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+		<h1 itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
 	</header>
 	
 	<footer>
-		<p>Posted on <time datetime="<?php the_time( "c" ) ?>"><?php the_time( "j F Y" ) ?></time> by <?php the_author_meta( "display_name" ); ?></p>
+		<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
 		<?php if ( comments_open() ) : ?>
 			<p><?php comments_popup_link( "No comments", "1 comment", "% comments", "", "Comments off" ); ?></p>
 		<?php endif; ?>
 	</footer>
 
-	<?php the_excerpt(); ?>
+	<div itemprop="description"><?php the_excerpt(); ?></div>
 </article>
