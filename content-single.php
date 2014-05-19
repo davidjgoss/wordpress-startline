fd47355721f5e9cd9a5aa1a598e070a2d972723b
David Goss <david@davidgoss.co.uk>
2014-05-20 00:16:01 +0100

    Improve post author markup structure

--- content-single.php
+++ content-single.php
@@ -12,7 +12,7 @@
 	</header>
 	
 	<footer>
-		<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <span itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url name"><?php the_author(); ?></a></span></p>
+		<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
 		<p>Posted in <?php echo get_the_category_list( ", ", "", get_the_ID() ); ?>.</p>
 		<?php echo get_the_tag_list( "<p>Tagged with ", ", ", ".</p>" ); ?>
 		<?php if ( comments_open() ) : ?>
