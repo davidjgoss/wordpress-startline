aa1b4451add4f7a6bed465d10670085b7cb47f2c
David Goss <david@davidgoss.co.uk>
2014-05-19 23:38:27 +0100

    Added lists of categories and tags to single post template

--- content-single.php
+++ content-single.php
@@ -13,6 +13,8 @@
 	
 	<footer>
 		<p>Posted on <time datetime="<?php the_time( "c" ) ?>"><?php the_time( "j F Y" ) ?></time> by <?php the_author_posts_link(); ?></p>
+		<p>Posted in <?php echo get_the_category_list( ", ", "", get_the_ID() ); ?>.</p>
+		<?php echo get_the_tag_list( "<p>Tagged with ", ", ", ".</p>" ); ?>
 		<?php if ( comments_open() ) : ?>
 			<p><?php comments_popup_link( "No comments", "1 comment", "% comments", "", "Comments off" ); ?></p>
 		<?php endif; ?>
