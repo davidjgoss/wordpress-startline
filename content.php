613f108ed5ff8ff9276a3076b2c47008eaa80aa6
David Goss <david@davidgoss.co.uk>
2014-05-20 00:09:02 +0100

    Remove metadata from unspecified content template

--- content.php
+++ content.php
@@ -6,17 +6,10 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>">
+<article>
 	<header>
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 	</header>
-	
-	<footer>
-		<p>Posted on <time datetime="<?php the_time( "c" ) ?>"><?php the_time( "j F Y" ) ?></time> by <?php the_author_meta( "display_name" ); ?></p>
-		<?php if ( comments_open() ) : ?>
-			<p><?php comments_popup_link( "No comments", "1 comment", "% comments", "", "Comments off" ); ?></p>
-		<?php endif; ?>
-	</footer>
 
-	<?php the_excerpt(); ?>
+	<?php the_content(); ?>
 </article>
