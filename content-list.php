9cb8d1f264ded0bda20d939015a989a3d33205e3
David Goss <david@davidgoss.co.uk>
2014-05-21 00:48:55 +0100

    Simplified markup for blog post lists, changed headline from h1 to h2'

--- content-list.php
+++ content-list.php
@@ -7,16 +7,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" itemscope itemtype="http://schema.org/BlogPosting">
-	<header>
-		<h1 itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
-	</header>
-	
-	<footer>
-		<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
-		<?php if ( comments_open() ) : ?>
-			<p><a href="<?php the_permalink(); ?>#comments"><span itemprop="commentCount"><?php comments_number( "0", "1", "%" ); ?></span> comments</a></p>
-		<?php endif; ?>
-	</footer>
+	<h2 itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h2>
+
+	<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
+	<?php if ( comments_open() ) : ?>
+		<p><a href="<?php the_permalink(); ?>#comments"><span itemprop="commentCount"><?php comments_number( "0", "1", "%" ); ?></span> comments</a></p>
+	<?php endif; ?>
 
 	<div itemprop="description"><?php the_excerpt(); ?></div>
 </article>
