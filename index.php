05887a1f4de8431ce26f8b83bdfa442671fd5266
David Goss <david@davidgoss.co.uk>
2014-05-21 20:09:52 +0100

    Clean up layout of index template code

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
  * @package WordPress
  * @subpackage Startline
  * @since Startline 0.9
@@ -23,7 +14,9 @@ get_header(); ?>
 	<?php endwhile; ?>
 
 <?php else : ?>
+
 	<?php get_template_part( "content", "none" ); ?>
-<?php endif; // end have_posts() check ?>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
