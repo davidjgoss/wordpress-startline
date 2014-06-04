c1d6a04302f28736c117bba18a329e35d57ebe23
David Goss <david@davidgoss.co.uk>
2014-06-04 23:09:16 +0100

    Add Microformats 2 markup for posts, fixes #2

--- content-list.php
+++ content-list.php
@@ -5,13 +5,13 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" itemscope itemtype="http://schema.org/BlogPosting">
-	<h2 itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h2>
+<article id="post-<?php the_ID(); ?>" class="h-entry" itemscope itemtype="http://schema.org/BlogPosting">
+	<h2 class="p-name" itemprop="headline"><a class="u-url" href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h2>
 
-	<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
+	<p>Posted on <time class="dt-published" datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em class="p-author h-card" itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="u-url" href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span class="p-name" itemprop="name"><?php the_author(); ?></span></a></em></p>
 	<?php if ( comments_open() ) : ?>
 		<p><a href="<?php the_permalink(); ?>#comments"><span itemprop="commentCount"><?php comments_number( "0", "1", "%" ); ?></span> comments</a></p>
 	<?php endif; ?>
 
-	<div itemprop="description"><?php the_excerpt(); ?></div>
+	<div class="p-summary" itemprop="description"><?php the_excerpt(); ?></div>
 </article>
--- content-single.php
+++ content-single.php
@@ -5,19 +5,19 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" itemscope itemtype="http://schema.org/BlogPosting">
+<article id="post-<?php the_ID(); ?>" class="h-entry" itemscope itemtype="http://schema.org/BlogPosting">
 	<header>
-		<h1 itemprop="headline"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
+		<h1 class="p-name" itemprop="headline"><a class="u-url" href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
 	</header>
 	
 	<footer>
-		<p>Posted on <time datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em itemprop="author" itemscope itemtype="http://schema.org/Person"><a href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span itemprop="name"><?php the_author(); ?></span></a></em></p>
+		<p>Posted on <time class="dt-published" datetime="<?php the_time( "c" ) ?>" itemprop="datePublished"><?php the_time( "j F Y" ) ?></time> by <em class="p-author h-card" itemprop="author" itemscope itemtype="http://schema.org/Person"><a class="u-url" href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" itemprop="url"><span class="p-name" itemprop="name"><?php the_author(); ?></span></a></em></p>
 		<?php startline_category_list( get_the_ID(), ", ", "<p>Posted in ", ".</p>" ); ?>
 		<?php startline_tag_list( get_the_ID(), ", ", "<p>Tagged with ", ".</p>" ); ?>
 		<p><a href="#comments"><span itemprop="commentCount"><?php comments_number( "0", "1", "%" ); ?></span> comments</a></p>
 	</footer>
 
-	<div itemprop="articleBody"><?php the_content(); ?></div>
+	<div class="e-content" itemprop="articleBody"><?php the_content(); ?></div>
 
 	<?php comments_template(); ?>
 </article>
