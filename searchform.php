9cac08a1c1d905edc28862dc254529565a5bf593
David Goss <david@davidgoss.co.uk>
2014-06-04 22:53:01 +0100

    Forgot to add php prelude to search form

--- searchform.php
+++ searchform.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Startline
+ */
+?>
+
 <form method="get" action="<?php echo home_url( "/" ); ?>" role="search" >
 	<label id="search-label" for="search-field">Search</label>
 	<input id="search-field" type="search" name="s" placeholder="search site" autocorrect="off" autocapitalize="off" />
