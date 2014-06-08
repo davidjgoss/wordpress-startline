2e0a5f92a88c25a0c64eba8deea31c454b573a63
David Goss <david@davidgoss.co.uk>
2014-06-08 08:41:20 +0100

    Changed search results content template to titles only

--- search.php
+++ search.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( "content", "list" ); ?>
+			<?php get_template_part( "content", "titles" ); ?>
 
 		<?php endwhile; ?>
 
